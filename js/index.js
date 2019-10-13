0c369a21fecc5da906ba8c4b05b4443e3a3f88a7
Yazeed aljuhani <yazeedmalmarwani@gmail.com>
2019-10-13 16:36:17 +0300

    added boring game logic

--- js/index.js
+++ js/index.js
@@ -1,57 +1,253 @@
-// let $obsticle = document.createElement('div')
-// $obsticle.classList.add("obs")
+// q learning 
 
-// document.body.appendChild($obsticle)
+// setInterval(function(){
+//     myMove()
+// },100)
+// myMove()
 
-// $obsticle.addEventListener("mousemove", func);
-// function func(){
-//     alert('Game Over :(')
-    
-// }
-// function myMove() {
-//     var elem = document.getElementById("animate");   
-//     var pos = 0;
-//     var id = setInterval(frame, 5);
-//     function frame() {
-//       if (pos == 350) {
-//         clearInterval(id);
-//       } else {
-//         pos++; 
-//         elem.style.top = pos + "px"; 
-//         elem.style.left = pos + "px"; 
-//       }
-//     }
-//   }
-setInterval(function(){
-    myMove()
-},100)
-function myMove() {
-    let upper = [100]
+// code from stack overflow 
+var reset = 5000
+setInterval(
+  start, reset
+  );
 
-    var elem = document.querySelectorAll(".animate");
-    console.log(elem);
-    
-    elem.forEach(function(el){
-        y = upper[upper.length - 1]
-        el.style.top = y + "px"; 
-        upper.push(y+100)
-        el.addEventListener("mousemove", func);
-        var pos = 0;
+function getWidth() {
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  );
+}
+
+function getHeight() {
+  return Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.documentElement.clientHeight
+  );
+}
+
+// end of code from stack overflow 
+let left = function(velocity, num){
+let upper = [100]
+
+  console.log('here');
+  // var elem = document.querySelectorAll(".animate");
+
+  for (let index = 0; index < num; index++) {
+    $elem = document.createElement('div')
+    $elem.setAttribute("class", "animate");
+    document.body.appendChild($elem)
+  }
+
+  var elem = document.querySelectorAll(".animate");
+  // console.log(elem);
+
+  elem.forEach(function (el) {
+    y = upper[upper.length - 1]
+    el.style.top = y + "px";
+    upper.push(y + 100)
+    el.addEventListener("mousemove", func);
+    var pos = 0;
     var id = setInterval(frame, 5);
     function frame() {
-      if (pos == 1000) {
+      if (pos >= 1360) {
         clearInterval(id);
+        el.remove()
+        // console.log('removed');
+        // alert("done")
+        console.log("no stop")
+        // pos =0  ern
+        // start()
+
+
+
       } else {
-        pos++; 
-     
-        el.style.left = pos + "px"; 
+        pos += velocity; 
+        // console.log('working -_-');
+        // console.log(getWidth());
+
+        el.style.left = pos + "px";
       }
     }
   })
-    
+// start()
 }
-      
-    
-  function func(){
-      alert('Game Over :(')
+let bottom = function(velocity, num){
+  let upper = [100]
+  
+    console.log('here');
+    // var elem = document.querySelectorAll(".animate");
+  
+    for (let index = 0; index < num; index++) {
+      $elem = document.createElement('div')
+      $elem.setAttribute("class", "animate");
+      document.body.appendChild($elem)
+    }
+  
+    var elem = document.querySelectorAll(".animate");
+    // console.log(elem);
+  
+    elem.forEach(function (el) {
+      y = upper[upper.length - 1]
+      el.style.left = y + "px";
+      upper.push(y + 100)
+      el.addEventListener("mousemove", func);
+      var pos = 0;
+      var id = setInterval(frame, 5);
+      function frame() {
+        if (pos >= 1360) {
+          clearInterval(id);
+          el.remove()
+          // console.log('removed');
+          // alert("done")
+          console.log("no stop")
+          // pos =0  ern
+          // start()
+  
+  
+  
+        } else {
+          pos += velocity; 
+          // console.log('working -_-');
+          // console.log(getWidth());
+  
+          el.style.bottom = pos + "px";
+        }
+      }
+    })
+  // start()
   }
+let right = function(velocity, num){
+  let upper = [100]
+  
+    console.log('here');
+    // var elem = document.querySelectorAll(".animate");
+  
+    for (let index = 0; index < num; index++) {
+      $elem = document.createElement('div')
+      $elem.setAttribute("class", "animate");
+      document.body.appendChild($elem)
+    }
+  
+    var elem = document.querySelectorAll(".animate");
+    // console.log(elem);
+  
+    elem.forEach(function (el) {
+      y = upper[upper.length - 1]
+      el.style.top = y + "px";
+      upper.push(y + 100)
+      el.addEventListener("mousemove", func);
+      var pos = 0;
+      var id = setInterval(frame, 5);
+      function frame() {
+        if (pos >= 1360) {
+          clearInterval(id);
+          el.remove()
+          // console.log('removed');
+          // alert("done")
+          console.log("no stop")
+          // pos =0  ern
+          // start()
+  
+  
+  
+        } else {
+          pos += velocity; 
+          // console.log('working -_-');
+          // console.log(getWidth());
+  
+          el.style.right = pos + "px";
+        }
+      }
+    })
+  // start()
+  }
+  let fook = function(velocity, num){
+    let upper = [0]
+    
+      console.log('here');
+      // var elem = document.querySelectorAll(".animate");
+    
+      for (let index = 0; index < num; index++) {
+        $elem = document.createElement('div')
+        $elem.setAttribute("class", "animate");
+        document.body.appendChild($elem)
+      }
+    
+      var elem = document.querySelectorAll(".animate");
+      // console.log(elem);
+    
+      elem.forEach(function (el) {
+        y = upper[upper.length - 1]
+        el.style.left = y + "px";
+        upper.push(y + 100)
+        el.addEventListener("mousemove", func);
+        var pos = 0;
+        var id = setInterval(frame, 5);
+        function frame() {
+          if (pos >= 1360) {
+            clearInterval(id);
+            el.remove()
+            // console.log('removed');
+            // alert("done")
+            console.log("no stop")
+            // pos =0  ern
+            // start()
+    
+    
+    
+          } else {
+            pos += velocity; 
+            // console.log('working -_-');
+            // console.log(getWidth());
+    
+            el.style.top = pos + "px";
+          }
+        }
+      })
+    // start()
+    }
+let num = 0
+
+let speed = 2 ;
+let count = 0 
+function start(){
+var arr = [fook,bottom,left,right];
+  let vel = 1360/speed 
+  reset = 500
+  
+  num = Math.round(Math.random() * (10) + 5)  
+
+  ranDir = Math.round(Math.random() * (3 - 0) + 1)  
+  speed = speed + 0.4
+  if (speed > 50){
+    speed = 10
+  }
+  count++
+  arr[ranDir](speed, num)
+  if(count > 13){
+    reset = 2000
+  }else if(count > 22){
+    reset = 1000
+  }else if(count > 50){
+    reset = 500
+  }
+  
+}
+
+
+
+
+function myMove() {
+  start()
+}
+
+
+function func() {
+  alert('Game Over :(')
+}
