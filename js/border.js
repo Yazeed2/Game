c821cee2b0fdebf89766d59678e3e41fa81be56b
Yazeed aljuhani <yazeedmalmarwani@gmail.com>
2019-10-16 09:09:06 +0300

    super hard game for ahmed

--- js/border.js
+++ js/border.js
@@ -1,4 +1,4 @@
-
+//make the borders so that no one can get away from the playing area 
 $fixed = document.getElementById('fixed')
 
 $fixed.addEventListener("mouseover", func);
@@ -15,15 +15,18 @@ $fixed = document.getElementById('topfixed')
 
 $fixed.addEventListener("mouseover", func);
 
+//game over to desplay the score and reloads the page
 function func() {
     alert('Game Over :(. your score is :'+ score)
     location.reload();
   }
+//this hides the game  
 $main = document.getElementById('main')
 $main.setAttribute('style', 'display:none;')
 
 $starting = document.getElementById('starting')
 
+//displays the game and starts it 
 function play(){
 $main.setAttribute('style', 'display:block;')
 $starting.setAttribute('style', 'display:none;')
